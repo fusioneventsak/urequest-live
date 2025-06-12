3de209fc22bbad46215bd4b8e09e49a6343fcae6
fusioneventsak <arthurk@fusion-events.ca>
2025-06-11 22:05:55 -0400

    Update Public View Button Text

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+@@ .. @@
+                  <button 
+                    onClick={navigateToFrontend}
+                    className="neon-button"
+                  >
+-                    Exit to Public View
++                    Public View
+                  </button>
+                  <button 
+                    onClick={navigateToKiosk}
+                    className="neon-button"
+                  >
